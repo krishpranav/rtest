54bcf57b20dff76498e49670f187d1054a59c00f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-10 18:11:01 +0530

    lib + assertion: assertions[added various asserts]

---
+++ src/assertion.rs
@@ -0,0 +1,79 @@
+use std::cmp::PartialEq;
+use std::fmt::Debug;
+use std::{panic::{catch_unwind, UnwindSafe, set_hook, take_hook}};
+
+pub struct Expect<T>
+    where
+        T: PartialEq + Debug,
+{
+    pub result: T
+}
+
+impl<T> Expect<T>
+    where
+        T: PartialEq + Debug,
+{
+    pub fn new(expect: T) -> Expect<T> {
+        Expect { result: expect }
+    }
+
+    pub fn expect(result: T) -> Expect<T> {
+        Expect { result }
+    }
+
+    pub fn equals(&self, control: T) -> Result<(), String> {
+        if self.result == control {
+            Ok(())
+        } else {
+            Err(format!("Expected "))
+        }
+    }
+
+    pub fn to_equals(&self, control: T) -> Result<(), String> {
+        self.equals(control)
+    }
+
+    pub fn to_be(&self, control: T) -> Result<(), String> {
+        if self.result == control {
+            Ok(())
+        } else {
+            Err(format!("Expected "))
+        }
+    }
+
+    pub fn to_not_equal(&self, control: T) -> Result<(), String> {
+        if self.result != control {
+            Ok(())
+        } else {
+            Err(format!("Expected "))
+        }
+    }
+
+    pub fn should_panic<T: FnOnce() + UnwindSafe>(closure: T) -> Result<(), String> {
+        set_hook(Box::new(|_| {}));
+
+        let result = catch_unwind(|| closure());
+
+        let _ = take_hook();
+
+        if result.is_ok() {
+            Err("Expected to panic!".to_string())
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn should_not_panic<T: FnOnce() + UnwindSafe>(closure: T) -> Result<(), String> {
+        set_hook(Box::new(|_| {}));
+
+        let result = catch_unwind(|| closure());
+
+        let _ = take_hook();
+
+        if result.is_ok() {
+            Ok(())
+        } else {
+            Err("Expected to panic!".to_string())
+        }
+    }
+}
